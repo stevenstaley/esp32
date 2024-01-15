2cb64c69d3b3e12e70be6045c952588732bc205e
stevenstaley <73314563+stevenstaley@users.noreply.github.com>
2024-01-15 13:43:02 -0500

    Update esp32mqtt.py

--- esp32mqtt.py
+++ esp32mqtt.py
@@ -1,29 +1,16 @@
+from umqtt.simple import MQTTClient
+from machine import ADC, Pin
+import time
+#from soilmoisture import read_soil_moisture
+
 broker_address = "192.168.0.211" # Replace with the IP address of the Raspberry Pi
 client_id = "ESP32_Subscriber"
-topic = "test/topic"
+topic_pub = "publish/esp32"
 
-# Callback function to receive messages
-def on_message(topic, msg):
-    print(f"Received message: {msg} from topic: {topic}")
-
-# Create a new MQTT client instance
 client = MQTTClient(client_id, broker_address, keepalive=60)
 client.connect()
 
-# Connect to the broker
+message = 'hello from ESP32 did this change'
 
-client.set_callback(on_message)
-# Subscribe to the topic
 while True:
-    client.publish(topic)
-
-print("ESP32 Subscriber connected to MQTT broker, subscribed to topic:", topic)
-
-# Listen for messages
-try:
-    while True:
-        client.check_msg()
-        time.sleep(1)
-except KeyboardInterrupt:
-    client.disconnect()
-    print("Subscriber script terminated.")
+    client.publish(topic_pub, message)
